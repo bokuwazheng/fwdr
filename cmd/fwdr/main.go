c2a4343a035a38f748eac341069b70f0cb042e53
bokuwazheng <yevgeny.kolomensky@gmail.com>
2022-08-04 02:05:13 +0300

    init

---
+++ cmd/fwdr/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+var (
+	self     string = os.Getenv("SELF")
+	botToken string = os.Getenv("BOT_TOKEN")
+	chatID   int64
+)
+
+func init() {
+	i, err := strconv.ParseInt(os.Getenv("CHAT_ID"), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	chatID = i
+}
+
+// TODO: keep online appearance
+func main() {
+	s, _ := discordgo.New(self)
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Discord selfbot (%s) is ready", r.User.Username)
+	})
+
+	bot := bot(botToken)
+	s.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			log.Fatalf("Cannot open the session: %v", err)
+		}
+		if c.Type != discordgo.ChannelTypeDM && c.Type != discordgo.ChannelTypeGroupDM {
+			return
+		} else if m.Author.ID == s.State.User.ID {
+			return
+		}
+		msg := tgbotapi.NewMessage(chatID, fmt.Sprintf("New Discord message from %s: %s", m.Author, m.Content))
+		bot.Send(msg)
+	})
+
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer s.Close()
+
+	ticker := time.NewTicker(10 * time.Minute)
+	defer ticker.Stop()
+	go func() {
+		for range ticker.C {
+			log.Println("Settings Status to idle!")
+			s.UpdateStatusComplex(discordgo.UpdateStatusData{Status: "online"})
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Graceful shutdown")
+}
+
+func bot(token string) *tgbotapi.BotAPI {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Telegram bot authorized on account %s", bot.Self.UserName)
+	return bot
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/bokuwazheng/fwdr
+
+go 1.18
+
+require github.com/bwmarrin/discordgo v0.25.0
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
